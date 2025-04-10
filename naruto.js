165f052314c86138fdbe38721786f0486222b029
sofiaas16 <sarahzambrano2019@gmail.com>
2025-04-10 01:01:10 -0500

    feat: :lipstick: CSS
    Le agregue diseño al CSS

--- naruto.js
+++ naruto.js
@@ -1,10 +1,17 @@
+// 1. Se crea una función ASÍNCRONA que se llama apiNaruto, para conectarme al API de Naruto
 const apiNaruto = async () => {
+
+    // Guardo la URL de la API en una variable
     let url = "https://dattebayo-api.onrender.com/characters";
+
+    // Uso fetch para hacer la petición a la API (como pedirle los datos al servidor)
     const response = await fetch(url);
+
+    // Se transformaa la response a JSON
     const data = await response.json();
     console.log(data);
 
-    // ALL
+    // En este caso, elegí el contenedor all para almacenar todos los personajes
     const all = document.querySelector(".all");
     data.characters.slice(0,8).forEach((item) => {
         const divItem = document.createElement('div');
@@ -21,9 +28,11 @@ const apiNaruto = async () => {
                 </div>
             </div>
         `;
+
+        // Meto ese div que acabo de crear dentro del contenedor principal (.all)
         all.appendChild(divItem);
     });
 };
 
-
+// Que NO SE ME OLVIDE llamar a la función para que se ejecute al cargar la página y se muestren los personajes
 apiNaruto();
